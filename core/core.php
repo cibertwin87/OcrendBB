d38e2817b7566a9f4208055d383ffd69abd3a606
Cybertwin <cybertwins87@gmail>
2017-04-27 18:05:56 +0200

    entorno creado

---
+++ core/core.php
@@ -0,0 +1,9 @@
+<?php
+
+define('HTML_DIR', 'html/');
+define('PAGE_TITTLE', 'OcrendBB');
+define('APP_COPY', 'Copyright &copy'. date('Y' , time()) . ' Ocrend Software.' );
+
+require('vendor/autoload.php');
+
+ ?>
